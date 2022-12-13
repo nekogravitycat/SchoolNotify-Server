2bc9d5035462208c3e1b780680100e1eb2155100
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2022-12-13 11:34:22 +0800

    fix crontab

--- crontab/run_daily.sh
+++ crontab/run_daily.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "SchoolNotify: daily schedule run at $(date)"
+cd /home/gravity/SchoolNotify-Server
 docker compose exec schoolnotify python -m src.daily
 echo "SchoolNotify: daily schedule finish at $(date)"
